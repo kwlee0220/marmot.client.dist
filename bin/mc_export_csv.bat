439009f198b580f5381b24aa2702702cd390ce49
kwlee <kwlee@etri.re.kr>
2017-09-08 10:28:35 +0900

    .

--- bin/mc_export_csv.bat
+++
@@ -1,1 +0,0 @@
-java -cp %MARMOT_CLIENT_HOME%/bin/marmot.client.jar marmot.command.ExportAsCsvFile -host %MARMOT_HOST% -port %MARMOT_PORT% %*
---
+++ bin/windows/mc_rename.bat
@@ -0,0 +1,1 @@
+java -cp %MARMOT_CLIENT_HOME%/bin/marmot.client.jar marmot.command.RenameDataSet -host %MARMOT_HOST% -port %MARMOT_PORT% %*
