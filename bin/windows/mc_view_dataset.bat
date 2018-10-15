9149dcfa9206b66ae5f44104e1703b7299b2fc2e
kwlee <kwlee@etri.re.kr>
2018-10-15 15:47:03 +0900

    .

--- bin/windows/mc_view_dataset.bat
+++ bin/windows/mc_view_dataset.bat
@@ -1,5 +1,5 @@
 @echo off
 
 java -cp %MARMOT_CLIENT_HOME%/bin/marmot.client.jar ^
-marmot.geo.command.RemoteViewDataSetMainMain ^
+marmot.geo.command.RemoteViewDataSetMain ^
 -host %MARMOT_HOST% -port %MARMOT_PORT% %*
