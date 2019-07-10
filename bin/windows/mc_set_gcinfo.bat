a35d007f6f9c4744143319517d4ddf59205e467a
kwlee <kwlee@etri.re.kr>
2019-07-10 16:59:31 +0900

    .

--- bin/windows/mc_set_gcinfo.bat
+++ bin/windows/mc_set_gcinfo.bat
@@ -1,5 +1,5 @@
 @echo off
 
 java -cp %MARMOT_CLIENT_HOME%\bin\marmot.client.jar ^
-marmot.command.explorer.DataSetExplorer ^
+marmot.geo.command.RemoteSetGeometryColumnInfoMain ^
 -host %MARMOT_HOST% -port %MARMOT_PORT% %*
